5671aaee32229640947e2dd3fdad07328ed76298
Patrick Barron <barronpm@gmail.com>
2021-02-15 17:51:08 -0500

    Add ItemRemoved event

---
+++ MediaBrowser.Controller/Events/Library/ItemRemovedEventArgs.cs
@@ -0,0 +1,9 @@
+namespace MediaBrowser.Controller.Events.Library
+{
+    /// <summary>
+    /// An event that occurs when an item is removed from a library.
+    /// </summary>
+    public class ItemRemovedEventArgs : ItemChangedEventArgs
+    {
+    }
+}

7a51f61f87ee16e9b0f000f8f2519c5a70908c20
Patrick Barron <barronpm@gmail.com>
2021-02-15 17:50:59 -0500

    Add ItemAdded event

---
+++ MediaBrowser.Controller/Events/Library/ItemAddedEventArgs.cs
@@ -0,0 +1,9 @@
+namespace MediaBrowser.Controller.Events.Library
+{
+    /// <summary>
+    /// An event that occurs when an item is added to a library.
+    /// </summary>
+    public class ItemAddedEventArgs : ItemChangedEventArgs
+    {
+    }
+}
